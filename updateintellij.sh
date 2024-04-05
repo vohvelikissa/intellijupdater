2fdc9167a8f290ce24b78aeb9f512c029323d51b
Pessi <--add>
2024-04-05 10:41:15 +0300

    update

--- updateintellij.sh
+++ updateintellij.sh
@@ -2,6 +2,8 @@ perkele=$(pwd)
 cd 
 mkdir tools
 cd tools
+rm -r idea-*
+rm -r intellij
 wget https://download.jetbrains.com/idea/ideaIC-2023.3.6.tar.gz
 mv *.tar.gz intellij.tar.gz
 tar -xf intellij.tar.gz
